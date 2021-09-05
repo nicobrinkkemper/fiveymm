288688a424a24cac6ac71657c5084127cd881dfc
Nico Brinkkemper <Nico.Brinkkemper@dpgmedia.nl>
2021-09-06 00:10:19 +0200

    added test for levels and added csvtojson

--- src/Level.test.tsx
+++ src/Level.test.tsx
@@ -1,27 +1,26 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import AppWrapper from './AppWrapper';
-import { MemoryRouter } from 'react-router-dom';
 import { useLevelData, startDate } from 'useLevelData';
 import formatDate from 'formatBatchName';
 
 test('can show level codes', () => {
   const levelData = useLevelData()
-  render(<MemoryRouter><AppWrapper /></MemoryRouter>);
+  render(<AppWrapper />);
   fireEvent.click(screen.getByText(/To the levels/i));
   fireEvent.click(screen.getByText(formatDate(startDate)));
   const [level] = levelData.levels(1)
   fireEvent.click(screen.getByText(level.levelName));
-  const levelCode = screen.getByText(level.levelCode.toUpperCase());
+  const levelCode = screen.getByText(level.levelCode);
   expect(levelCode).toBeInTheDocument();
 });
 
 
 test('can show maker ids', () => {
   const levelData = useLevelData()
-  render(<MemoryRouter><AppWrapper /></MemoryRouter>);
+  render(<AppWrapper />);
   const [level] = levelData.levels(1)
   fireEvent.click(screen.getByText(level.levelName));
-  const makerId = screen.getByText(level.makerId.toUpperCase());
+  const makerId = screen.getByText(level.makerId);
   expect(makerId).toBeInTheDocument();
 });
